e20f2007f0fd2e33221798b31543fec3e8ceaa43
warlord1352 <steve98890@yahoo.com>
2020-07-22 12:09:32 -0700

    updated items.csv, added bat to start AH.bat

--- start AH.bat
+++ start AH.bat
@@ -2,7 +2,7 @@
 :onCrash
 echo [%date% %time%] Starting AH Tool...
 cd "D:\EVERYTHING FFXI\My Topaz Server\pytopaz\bin"
-"C:\Users\steve\Anaconda3\python.exe" .\broker.py
+"C:\Users\steve\AppData\Local\Programs\Python\Python37\python.exe" .\broker.py --verbose items.csv
 @ECHO AH-Bot stopped or crashed!
 @ECHO %date% %time%> ..\Last_AH-tool_Stop.log
 PAUSE
